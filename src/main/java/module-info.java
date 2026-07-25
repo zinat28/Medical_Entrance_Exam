883af50a80308bbfb4f6f6037a12233ffc92bd1a
Tanvir2220338 <2220338@iub.edu.bd>
2026-07-25 17:22:00 +0600

    Add SQLite dependency and SQL module by Tanvir
    # Conflicts:
    #	src/main/java/com/summer/section1/group4/medical_entrance_exam/tanvir/LoginController.java
    #	src/main/java/module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,12 @@ module com.summer.section1.group4.medical_entrance_exam {
     requires javafx.fxml;
     requires javafx.graphics;
     requires java.desktop;
-
+    requires java.sql;
 
     opens com.summer.section1.group4.medical_entrance_exam to javafx.fxml;
     opens com.summer.section1.group4.medical_entrance_exam.zinat to javafx.fxml;
     opens com.summer.section1.group4.medical_entrance_exam.zinat.Applicant to javafx.fxml;
     opens com.summer.section1.group4.medical_entrance_exam.zinat.QuestionModerator to javafx.fxml;
-
 
     exports com.summer.section1.group4.medical_entrance_exam;
 }
