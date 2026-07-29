9c6c0f305ddaaabfa9de8049ea5f6c3c8a80ef11
Tanvir2220338 <2220338@iub.edu.bd>
2026-07-29 18:01:25 +0600

    Add database initializer and table creation test

---
+++ src/main/java/com/summer/section1/group4/medical_entrance_exam/tanvir/user7user8/database/DatabaseInitializerTest.java
@@ -0,0 +1,17 @@
+package com.summer.section1.group4.medical_entrance_exam.tanvir.user7user8.database;
+
+public class DatabaseInitializerTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Starting database table initialization..."
+        );
+
+        DatabaseInitializer.initializeDatabase();
+
+        System.out.println(
+                "Database table test completed."
+        );
+    }
+}
