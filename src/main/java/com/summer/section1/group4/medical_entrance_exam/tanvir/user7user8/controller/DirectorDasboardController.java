5f089a9965d26f3cf3a08338cd96af93e209df16
Tanvir2220338 <2220338@iub.edu.bd>
2026-08-06 01:00:19 +0600

    Added working logout for medical officer and dashboard

--- src/main/java/com/summer/section1/group4/medical_entrance_exam/tanvir/user7user8/controller/DirectorDasboardController.java
+++
@@ -1,4 +0,0 @@
-package com.summer.section1.group4.medical_entrance_exam.tanvir.user7user8.controller;
-
-public class DirectorDasboardController {
-}
---
+++ src/main/java/com/summer/section1/group4/medical_entrance_exam/tanvir/user7user8/database/UserTableTest.java
@@ -0,0 +1,4 @@
+package com.summer.section1.group4.medical_entrance_exam.tanvir.user7user8.database;
+
+public class UserTableTest {
+}
